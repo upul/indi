963a43e10133cc3b96ac9d06213cdebe30a4db98
Upul Bandara <upulbandara@gmail.com>
2017-04-21 10:32:37 +0530

    feature: Decision Tree first cut done!

--- test/test_decision_tree.py
+++ test/test_decision_tree.py
@@ -30,22 +30,23 @@ def test_leaf_node_predict():
     output = leaf.predict_probability()
     npt.assert_array_equal(output, np.array([0.0, 0.6, 0.2, 0.2]))
 
-    X = np.array([[1, 1], [2, 1], [4, 1], [5, 1], [3, 10]])
-    y = np.array(([0, 0, 1, 1, 2]))
-    t = ClassificationTree(max_depth=100, n_min_leaf=1, n_trials=100)
-    t.fit(X, y)
-    t.visualize('./img.png')
-    print('O')
+    # X = np.array([[1, 1], [2, 1], [4, 1], [5, 1], [3, 10]])
+    # y = np.array(([0, 0, 1, 1, 2]))
+    # t = ClassificationTree(max_depth=100, n_min_leaf=None, n_trials=None)
+    # t.fit(X, y)
+    # t.visualize('./img.png')
+    # print('O')
 
     from sklearn.datasets.samples_generator import make_blobs
     import matplotlib.pylab as plt
     import seaborn as sbs;
-    n_samples = 1000
+    n_samples = 2500
     X, y = make_blobs(n_samples=n_samples, centers=2, n_features=2,
                       cluster_std=0.65, random_state=1788)
 
-    cls = ClassificationTree(max_depth=100, n_min_leaf=1, n_trials=100)
-    root = cls._fit_training_data(X, y)
+    cls = ClassificationTree()
+    cls.fit(X, y)
+    cls.visualize('./test.png')
 
     h = 0.02
     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
